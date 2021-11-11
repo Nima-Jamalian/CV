2478f7129d4bbd8fa3cbbb4a4ca6f29ecddd1f33
Nima Jamalian <nima.jamalian80@gmail.com>
2021-11-11 15:20:52 +0000

    Experience Section

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector(".southBankLogo").addEventListener("click", () =>{
+    var southBankTitle = document.querySelector(".southBankTitle");
+    var southBankList = document.querySelector(".southBankList");
+if(southBankList.classList.contains("hideItem")){
+    southBankList.classList.remove("hideItem");
+    southBankTitle.classList.add("boldText");
+} else{
+    southBankList.classList.add("hideItem");
+    southBankTitle.classList.remove("boldText");
+}
+});
+
+document.querySelector(".NokiaLogo").addEventListener("click", () =>{
+    var NokiaTitle = document.querySelector(".NokiaTitle");
+    var NokiaList = document.querySelector(".NokiaList");
+if(NokiaList.classList.contains("hideItem")){
+    NokiaList.classList.remove("hideItem");
+    NokiaTitle.classList.add("boldText");
+} else{
+    NokiaList.classList.add("hideItem");
+    NokiaTitle.classList.remove("boldText");
+}
+});
