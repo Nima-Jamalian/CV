8117e5e13c64a3c4d9d360fb52030fad60cf2d75
Nima Jamalian <nima.jamalian80@gmail.com>
2021-11-11 22:11:50 +0000

    welcome message

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.querySelector("#profilePic").addEventListener("mouseenter",()=>{
     document.querySelector("#profilePic").src = "images/NimaEmojiPhotoRound.png";
-    document.querySelector(".helloText").innerHTML = "Hello! 😁";
+    document.querySelector(".helloText").innerHTML = "Welcome to my site! 😁";
 
 });
 document.querySelector("#profilePic").addEventListener("mouseleave",()=>{
     document.querySelector("#profilePic").src = "images/Nima Profile Pciture.png";
-    document.querySelector(".helloText").innerHTML = "Hello.";
+    document.querySelector(".helloText").innerHTML = "Hello!";
 });
 
 document.querySelector(".southBankLogo").addEventListener("click", () =>{
